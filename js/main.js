d655d90b88bde60e4f44e246bb01a58bfb62bd8b
Ma Tingchen <defmys@gmail.com>
2018-10-06 16:59:45 +0800

    Force quit

--- js/main.js
+++ js/main.js
@@ -17,9 +17,7 @@ function createWindow () {
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+    app.quit()
 });
 
 app.on('activate', () => {
