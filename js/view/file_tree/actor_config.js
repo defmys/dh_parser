4212dbca99c3e4772d34fe1310bbd66ae37b4895
Ma Tingchen <defmys@gmail.com>
2018-10-12 12:47:35 +0800

    Support editing actor slots

--- js/view/file_tree/actor_config.js
+++ js/view/file_tree/actor_config.js
@@ -10,9 +10,11 @@ export class ActorConfig extends BaseConfig {
 
         this.handleAddSlot = this.handleAddSlot.bind(this);
         this.handleSlotIndexChange = this.handleSlotIndexChange.bind(this);
-        this.handleSlotValueChange = this.handleSlotValueChange.bind(this);
         this.handleSlotDisplayNameChange = this.handleSlotDisplayNameChange.bind(this);
         this.handleRemoveSlot = this.handleRemoveSlot.bind(this);
+        this.handleMaterialChange = this.handleMaterialChange.bind(this);
+        this.handleAddMaterial = this.handleAddMaterial.bind(this);
+        this.handleRemoveMaterial = this.handleRemoveMaterial.bind(this);
     }
 
     prepareConfigContent(content) {
@@ -28,7 +30,7 @@ export class ActorConfig extends BaseConfig {
                     let target = fixedContent['slots'][idx];
                     BaseConfig.fillWithDefault(target, from, 'index', idx);
                     BaseConfig.fillWithDefault(target, from, 'display_name', '');
-                    BaseConfig.fillWithDefault(target, from, 'value', 0);
+                    BaseConfig.fillWithDefault(target, from, 'materials', []);
                 }
             }
         }
@@ -77,16 +79,6 @@ export class ActorConfig extends BaseConfig {
         }
     }
 
-    handleSlotValueChange(event, idx) {
-        const value = parseInt(event.target.value);
-        let slots = {...this.state.slots};
-        if (slots[idx]) {
-            slots[idx].value = parseInt(event.target.value);
-
-            this.setState({slots: slots});
-        }
-    }
-
     handleSlotDisplayNameChange(event, idx) {
         const value = event.target.value;
         let slots = {...this.state.slots};
@@ -103,12 +95,59 @@ export class ActorConfig extends BaseConfig {
         this.setState({slots: slots});
     }
 
+    handleMaterialChange(event, idx, materialIdx) {
+        let slots = {...this.state.slots};
+        let slot = slots[parseInt(idx)];
+
+        slot.materials[parseInt(materialIdx)] = parseInt(event.target.value);
+
+        this.setState({slots: slots});
+    }
+
+    handleAddMaterial(idx) {
+        let slots = {...this.state.slots};
+        let slot = slots[parseInt(idx)];
+
+        slot.materials.push(0);
+
+        this.setState({slots: slots});
+    }
+
+    handleRemoveMaterial(idx, materialIdx) {
+        let slots = {...this.state.slots};
+        let slot = slots[parseInt(idx)];
+        slot.materials.splice(parseInt(materialIdx), 1);
+        this.setState({slots: slots});
+    }
+
+    renderMaterial(slotIndex) {
+        let buffer = [];
+
+        let slot = this.state.slots[slotIndex];
+        const material = slot.materials;
+        for (let idx in material) {
+            if (material.hasOwnProperty(idx)) {
+                const materialID = parseInt(material[idx]);
+                const key = `material_${idx.toString()}`;
+                buffer.push(<div className="row" key={key}>
+                    <div className="col">
+                        <input type="number" value={materialID} onChange={(event) => this.handleMaterialChange(event, slotIndex, idx)}/>
+                    </div>
+                    <div className="col-1">
+                        <button className="btn btn-outline-danger ml-1 mr-1" onClick={() => this.handleRemoveMaterial(slotIndex, idx)}><FontAwesomeIcon icon={faMinus}/></button>
+                    </div>
+                </div>);
+            }
+        }
+
+        return buffer;
+    }
+
     renderSlotItems() {
         let slotsBuffer = [];
         for (let index in this.state.slots) {
             if (this.state.slots.hasOwnProperty(index)) {
                 const idxName = `index_${index.toString()}`;
-                const valueName = `value_${index.toString()}`;
                 const displayName = `display_${index.toString()}`;
                 const slot = this.state.slots[index];
                 slotsBuffer.push(
@@ -117,7 +156,7 @@ export class ActorConfig extends BaseConfig {
                             <tr className="text-center">
                                 <th scope="col-2">ID</th>
                                 <th scope="col">Display Name</th>
-                                <th scope="col">Material ID</th>
+                                <th scope="col">Material ID <button className="btn btn-outline-primary ml-1 mr-1" onClick={() => this.handleAddMaterial(index)}><FontAwesomeIcon icon={faPlus}/></button></th>
                                 <th scope="col-1">
                                     <button className="btn btn-outline-danger ml-1 mr-1" onClick={() => this.handleRemoveSlot(index)}><FontAwesomeIcon icon={faMinus}/></button>
                                 </th>
@@ -135,19 +174,14 @@ export class ActorConfig extends BaseConfig {
                                     />
                                 </td>
                                 <td>
-                                    <input className="col"
-                                       name={displayName}
+                                    <input name={displayName}
                                        value={slot.display_name}
                                        onChange={(event) => this.handleSlotDisplayNameChange(event, index)}
                                     />
                                 </td>
+
                                 <td>
-                                    <input type="number"
-                                       className="col"
-                                       name={valueName}
-                                       value={slot.value}
-                                       onChange={(event) => this.handleSlotValueChange(event, index)}
-                                    />
+                                    {this.renderMaterial(index)}
                                 </td>
                             </tr>
                         </tbody>
